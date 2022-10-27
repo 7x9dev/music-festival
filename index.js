395fa81dba0cc1f91df33d72a37d560bc1afea8c
7x9dev <103652226+7x9dev@users.noreply.github.com>
2022-10-28 01:07:30 +0300

    Added countdown

--- index.js
+++ index.js
@@ -12,3 +12,35 @@ btn.addEventListener("click", function () {
       state = "play";
    }
 });
+
+function countdown() {
+   const festivalDate = new Date("October 19, 2023 00:00");
+   const now = new Date();
+   const timeLeft = festivalDate - now;
+
+   const msInSec = 1000;
+   const msInMin = 60 * 1000;
+   const msInHr = 60 * 60 * 1000;
+   const msInDay = 24 * 60 * 60 * 1000;
+
+   const displayDays = Math.floor(timeLeft / msInDay);
+   document.querySelector(".days").textContent = displayDays;
+
+   const displayHours = Math.floor((timeLeft % msInDay) / msInHr);
+   document.querySelector(".hours").textContent = displayHours;
+
+   const displayMin = Math.floor((timeLeft % msInHr) / msInMin);
+   document.querySelector(".min").textContent = displayMin;
+
+   const displaySec = Math.floor((timeLeft % msInMin) / msInSec);
+   document.querySelector(".sec").textContent = displaySec;
+
+   if (timeLeft <= 0) {
+      document.querySelector(".days").textContent = 0;
+      document.querySelector(".hours").textContent = 0;
+      document.querySelector(".min").textContent = 0;
+      document.querySelector(".sec").textContent = 0;
+      clearInterval(timerID);
+   }
+}
+let timerID = setInterval(countdown, 1000);
